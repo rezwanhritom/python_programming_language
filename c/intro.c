45f03815956725d3fed9afdf0823d64e2d3a2752
Rezwanur Rahman <rezwanhritom1537@gmail.com>
2026-01-11 23:20:21 +0600

    updated name and others

--- c/intro.c
+++
@@ -1,8 +0,0 @@
-// This is the first file of the c folder which will contain all the c files
-#include <stdio.h>
-int main() {
-    print("Welcome to the C folder!");
-    print("This folder will contain various C files for different purposes.");
-    print("Stay tuned for more updates!");
-    return 0;
-}
