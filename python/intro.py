73df89ccc288732c15edffec4bc00fc14f644a35
Rezwanur Rahman <rezwanhritom1537@gmail.com>
2026-01-11 23:16:29 +0600

    created python and c folder

---
+++ python/intro.py
@@ -0,0 +1,4 @@
+# This is the first file of the python folder which will contain all the python files.
+print("Welcome to the Python folder!") 
+print("This folder will contain various Python files for different purposes.")
+print("Stay tuned for more updates!")
